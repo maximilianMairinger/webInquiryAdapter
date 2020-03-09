d6f91b7d8f964f058a3dd6b8978bb71b9248e505
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2020-03-09 13:30:21 +0100

    work

--- app/src/webInquiryAdapter.ts
+++ app/src/webInquiryAdapter.ts
@@ -1,6 +1,127 @@
-import express from express
+import * as MongoDB from "mongodb"
+import { RequestHandler } from "express";
+import * as crypto from "crypto"
+const MongoClient = MongoDB.MongoClient;
+import Xrray from "xrray"
+Xrray(Array)
 
 
-export default function() {
+function createSessionKey() {
+  return crypto.createHash('sha512').update(Math.random().toString() + "d726aloskjhe8721").digest("hex").toUpperCase();
+}
+
+
+export default function(uri: string, auth?: {username: string, password: string}, dbname: string = "webinquiry"): RequestHandler {
   
+  let client: MongoDB.MongoClient
+  if (auth) {
+    uri = "mongodb://" + auth.username + ":" + auth.password + "@" + uri
+    client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+  }
+  else {
+    uri = "mongodb://" + uri
+    client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+  }
+  
+  
+  client.connect()
+
+
+  let db = client.db(dbname)
+
+  let session = db.collection("session")
+  let recording = db.collection("recording")
+  let recordingMeta = db.collection("recordingMeta")
+
+
+
+  const resolve = {
+    auth: async (body: Auth) => {
+      let sessKey = createSessionKey()
+      if (!body.name) body.name = "Recording " + ((await session.countDocuments({})) + 1)
+
+      await session.insertOne({
+        name: body.name,
+        defaultDelta: body.defaultDelta, 
+        defaultProps: body.defaultProps, 
+        sessKey
+      })
+
+      return {sessKey}
+    },
+    log: async (body: Log) => {
+      session.findOne({sessKey: body.sessKey})
+    }
+  }
+
+  return async function(req, res) {
+    if (resolve[(req.body as Auth | Log).req]) res.send(await resolve[(req.body as Auth | Log).req](req.body))
+  } as RequestHandler  
+}
+
+
+type Auth = {
+  req: "auth";
+  name?: string;
+  defaultProps: {
+      "keydown"?: {};
+      "mousemove"?: {};
+  } & {[key: string]: any};
+  defaultDelta: number;
+}
+type Log = {
+  req: "log";
+  sessKey: string;
+  data: {
+      "keydown"?: {
+          delta: number;
+          props: {
+              code: string;
+          };
+      }[];
+      "mousemove"?: ({
+          delta: number;
+          props: {
+              x: number;
+              y: number;
+          };
+      } | {
+          props: {
+              x: number;
+              y: number;
+          };
+          delta?: undefined;
+      } & {[key: string]: {
+        delta: number;
+        props: {
+            code: string;
+        };
+    }})[];
+  }
+}
+
+let auth = {
+  req: "auth",
+  defaultProps: {
+    "keydown": {},
+    "mousemove": {},
+  },
+  defaultDelta: 16
+};
+
+let log = {
+  reqe: "log",
+  sessKey: "abcd",
+  data: {
+    "keydown": [
+      {delta: 100000, props: {code: "Enter"}},
+      {delta: 100000, props: {code: "KeyK"}}
+    ],
+    "mousemove": [
+      {delta: 17, props: {x: 10, y: 14}},
+      {delta: 15, props: {x: 12, y: 12}},
+      {props: {x: 12, y: 22}},
+      {delta: 15, props: {x: 22, y: 12}},
+    ]
+  }
 }
--- test/src/test.ts
+++ test/src/test.ts
@@ -1,4 +1,41 @@
 import webInquiryAdapter from "./../../app/src/webInquiryAdapter"
-//const testElem = document.querySelector("#test")
+import * as express from "express"
+import * as bodyParser from "body-parser"
+import * as path from "path"
 
-webInquiryAdapter()
+const port = 8200;
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+// function sendFile(res, p) {
+//   res.sendFile(path.join(__dirname + "/" + p));
+// }
+
+// app.use('/dist', express.static('dist'))
+// app.use('/res', express.static('res'))
+
+// app.get('/', (req, res) => {
+//   sendFile(res, "index.html")
+// });
+
+
+
+// app.post("/rec", (req, res) => {
+//   setTimeout(() => {
+//     res.send('{"valid": true, "data": {"fullName": "Maximilian Mairinger", "class": "lehrer", "registered": [true, false, true, true]}}')
+//   }, 2000)
+// })
+
+
+app.listen(port);
+console.log("Listening on Port: " + port);
+
+
+let inq = webInquiryAdapter("localhost")
+
+app.post("/inq", inq)
+
+
+
